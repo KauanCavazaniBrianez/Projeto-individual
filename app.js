dc2f39bc4a536b27a002edb66b051dfc8deff084
KauanCavazaniBrianez <kauan.brianez@sptech.school>
2022-05-16 18:24:24 -0300

    first comit

---
+++ app.js
@@ -0,0 +1,31 @@
+// process.env.AMBIENTE_PROCESSO = "desenvolvimento";
+process.env.AMBIENTE_PROCESSO = "producao";
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA = 3333;
+
+var app = express();
+
+var indexRouter = require("./src/routes/index");
+var usuarioRouter = require("./src/routes/usuarios");
+var avisosRouter = require("./src/routes/avisos");
+var medidasRouter = require("./src/routes/medidas");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/usuarios", usuarioRouter);
+app.use("/avisos", avisosRouter);
+app.use("/medidas", medidasRouter)
+
+app.listen(PORTA, function () {
+    console.log(`Servidor da ShopFlux está rodando!\n 
+    Você está rodando sua aplicação em Ambiente de ${process.env.AMBIENTE_PROCESSO} \n
+    Acesse o caminho a seguir para visualizar: http://localhost:${PORTA}`);
+});
